e89fab8b267c6f18031f1a154fab74f85ce40181
yaso <ten.work400@gmail.com>
2026-02-27 17:39:24 +0300

    Инициализация проекта

--- main_page/urls.py
+++ main_page/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from . import views
 
 urlpatterns = [
-    path('', include('main_page.urls')),
+    path('register/',views.registerPage),
 ]
